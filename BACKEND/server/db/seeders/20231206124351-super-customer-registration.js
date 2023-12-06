e0de02b8de88ec2108c668566e8bd1e28338e7f8
abishek.paudel <abishek.paudel@swifttech.com.np>
2023-12-06 18:42:33 +0545

    Pr request check

--- BACKEND/server/db/seeders/20231206124351-super-customer-registration.js
+++ BACKEND/server/db/seeders/20231206124351-super-customer-registration.js
@@ -30,6 +30,28 @@ module.exports = {
             login_date:new Date().getTime(),
             created_date: new Date().getTime()
           },
+
+          {
+          
+            id:2,
+            uuid: v4(),
+            first_name: "Himal",
+            last_name:"Subedi",
+            email:"Himal@gmail.com",
+            mobile_number:"9862383579",
+            password:"$2b$10$K/F6Qt3.MBFuD/aPejWzbOmPN.9umSEJ3UKBgVhGea.i92xJkr3VS",  //Nepal@12345
+            customer_pin:"$2b$10$oLtdqxEj6KEyQKsVfSfAD.X6ZG5IfKB/2jtr2b1X1tYdCbjoqrFAu", // 1127
+            salt:"SUPER_CUSTOMER",
+            is_active:1,
+            is_delete:0,
+            customer_type:3,
+            is_blocked:0,
+            created_date:new Date().getTime(),
+            created_by:"ABISHEK PAUDEL",
+            created_by: 1,
+            login_date:new Date().getTime(),
+            created_date: new Date().getTime()
+          },
         ],
         {}
       );
