ae2964a6235f418fac5074bc3d05fd0066326898
abishek.paudel <abishek.paudel@swifttech.com.np>
2023-12-06 18:55:10 +0545

    victim and non victim users added

--- BACKEND/server/db/seeders/20231206124351-super-customer-registration.js
+++ BACKEND/server/db/seeders/20231206124351-super-customer-registration.js
@@ -38,13 +38,35 @@ module.exports = {
             first_name: "Himal",
             last_name:"Subedi",
             email:"Himal@gmail.com",
-            mobile_number:"9862383579",
-            password:"$2b$10$K/F6Qt3.MBFuD/aPejWzbOmPN.9umSEJ3UKBgVhGea.i92xJkr3VS",  //Nepal@12345
+            mobile_number:"9898989898",
+            password:"$2b$10$K/F6Qt3.MBFuD/aPejWzbOmPN.9umSEJ3UKBgVhGea.i92xJkr3VS",  //Nepal@12345  
             customer_pin:"$2b$10$oLtdqxEj6KEyQKsVfSfAD.X6ZG5IfKB/2jtr2b1X1tYdCbjoqrFAu", // 1127
             salt:"SUPER_CUSTOMER",
             is_active:1,
             is_delete:0,
-            customer_type:3,
+            customer_type:2,
+            is_blocked:0,
+            created_date:new Date().getTime(),
+            created_by:"ABISHEK PAUDEL",
+            created_by: 1,
+            login_date:new Date().getTime(),
+            created_date: new Date().getTime()
+          },
+
+          {
+          
+            id:3,
+            uuid: v4(),
+            first_name: "Gaurab",
+            last_name:"Bhatta",
+            email:"gaurab@gmail.com",
+            mobile_number:"9898989897",
+            password:"$2b$10$K/F6Qt3.MBFuD/aPejWzbOmPN.9umSEJ3UKBgVhGea.i92xJkr3VS",  //Nepal@12345  
+            customer_pin:"$2b$10$oLtdqxEj6KEyQKsVfSfAD.X6ZG5IfKB/2jtr2b1X1tYdCbjoqrFAu", // 1127
+            salt:"SUPER_CUSTOMER",
+            is_active:1,
+            is_delete:0,
+            customer_type:1,
             is_blocked:0,
             created_date:new Date().getTime(),
             created_by:"ABISHEK PAUDEL",
