2ac1079a732bd246de53a2c25b2c6ba4ee355fba
abishek.paudel <abishek.paudel@swifttech.com.np>
2023-12-06 18:25:43 +0545

    query optimized for checking normal and super customer

--- BACKEND/server/modules/survival_customer_onboard/methods/login_balance_humaity.js
+++ BACKEND/server/modules/survival_customer_onboard/methods/login_balance_humaity.js
@@ -1,64 +1,69 @@
 "use strict";
 const httpStatus = require('http-status');
-const { mysqlHelper,jwtHelper } = require("./../../../helpers");
-const { balanceHumanityValidator }= require("./../helpers");
+const { mysqlHelper, jwtHelper } = require("./../../../helpers");
+const { balanceHumanityValidator } = require("./../helpers");
 const bcrypt = require('bcrypt');
 
-  (() => {
+(() => {
 
-    module.exports = async (call,res) => {
+    module.exports = async (call, res) => {
 
-      try {
+        try {
 
 
-        let response = await balanceHumanityValidator.loginCustomer(call.body);
-        if (response.status !== httpStatus.OK) {
-          return res.status(400).json({ message: response.message });
-        }
-
-        let userExistCheck = await mysqlHelper.format(`SELECT uuid, customer_pin, password,email,mobile_number FROM sagar_test.balance_humanity_users WHERE mobile_number = "${call.body.mobileNumber}"`);
-        let [userExistCheckResult] = await mysqlHelper.query(userExistCheck);
-  
-        if (!userExistCheckResult || userExistCheckResult.length === 0) {
-            return res.status(400).json({ message: "user doesnt exist!" });
-        }
-        if(userExistCheckResult && userExistCheckResult.length > 0)
-        {
-          const match = await bcrypt.compare(call.body.password, userExistCheckResult[0].password);
-
-          if(match)
-          {
-            const jwtToken = jwtHelper.generateJWTToken(userExistCheckResult[0].uuid)
-            let customerData =
-            {
-                mobileNumber : userExistCheckResult[0].mobile_number,
-                email:userExistCheckResult[0].email
+            let response = await balanceHumanityValidator.loginCustomer(call.body);
+            if (response.status !== httpStatus.OK) {
+                return res.status(400).json({ message: response.message });
             }
 
-            if(jwtToken.success)
-            {
-                return res.status(200).json({ token:jwtToken, message: "login successfully!",customerData:JSON.stringify(customerData) });
+            let userExistCheck = await mysqlHelper.format(`SELECT uuid, customer_pin, password,email,mobile_number,
+        case when customer_type= 1 then "NormalCustomer"
+        when customer_type = 2 then "VictimCustomer"
+        when customer_type = 3 then "superCustomer" 
+        end as customerType 
+ 
+
+         FROM sagar_test.balance_humanity_users WHERE mobile_number = "${call.body.mobileNumber}"`);
+            let [userExistCheckResult] = await mysqlHelper.query(userExistCheck);
+
+            if (!userExistCheckResult || userExistCheckResult.length === 0) {
+                return res.status(400).json({ message: "User doesnt exist!" });
+            }
+            if (userExistCheckResult && userExistCheckResult.length > 0) {
+                const match = await bcrypt.compare(call.body.password, userExistCheckResult[0].password);
+
+                if (match) {
+                    const jwtToken = jwtHelper.generateJWTToken(userExistCheckResult[0].uuid)
+                    let customerData =
+                    {
+                        mobileNumber: userExistCheckResult[0].mobile_number,
+                        email: userExistCheckResult[0].email,
+                        customerType: userExistCheckResult[0].customerType
+                    }
+
+                    if (jwtToken.success) {
+                        return res.status(200).json({ token: jwtToken, message: "login successfully!", customerData: JSON.stringify(customerData) });
+                    }
+
+
+                }
+
+                else {
+                    return res.status(401).json({ message: "Invalid Mobile Number or password." });
+
+                }
+
             }
 
 
-          }
 
-          else{
-            return res.status(401).json({ message: "Invalid Mobile Number or password." });
-
-          }
-
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "Internal Server Error" });
         }
-
-
-
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-      }
 
     }
 
-  })
-  ();
+})
+    ();
 
