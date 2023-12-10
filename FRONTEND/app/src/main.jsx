26e5ae39bdb753cfcb05e6351fd0333d85be223f
abishek.paudel <abishek.paudel@swifttech.com.np>
2023-12-10 18:18:31 +0545

    frontend part init and completed

--- FRONTEND/app/src/main.jsx
+++ FRONTEND/app/src/main.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './pages/App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import SignUpApplication from './pages/sign_up.jsx'; // Ensure the correct case
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App/>,
+    path: '/',
+    element: <App />,
   },
   {
-    path:"home",
-    element:<div>Home</div>
-  }
+    path: 'submit',
+    element: <SignUpApplication/>
+  },
 ]);
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
