1e3132fe3d2281bbbf039da8d77f111436934008
Bibek Regmi <bibek.regmi@swifttech.com>
2023-12-27 16:57:26 +0545

    added to customer bucket db after inserting into bucket

--- BACKEND/server/modules/minio_image/upload_image.js
+++ BACKEND/server/modules/minio_image/upload_image.js
@@ -1,10 +1,12 @@
 "use strict";
 let minioHelper = require("../../helpers/minio_helper");
+const { createCustomerBucket } = require("../customer_bucket/methods");
 
 
 let uploadImage = async (req,res)=>{
     let bucketName = process.env.BUCKET_NAME;
     // let bucketName = req.body.bucketName;
+
     let image = req.file
 let bucketExists = await minioHelper.bucketExists(bucketName)
 if(!bucketExists.status){
@@ -18,7 +20,20 @@ let metaData = {
 try {
     let putIntoBucket = await minioHelper.putObject(bucketName,`${image.originalname}`, image.path, metaData)
 if(putIntoBucket.status){
+    let request = {
+        body :{
+            customer_id : req.body.customer_id,
+            image_name : image.originalname,
+            bucket_name: req.body.bucketName
+        }
+    }
+    let customerBucket = await createCustomerBucket(request);
+    if(customerBucket.status == 200)
+    {
      return res.status(200).json(putIntoBucket);
+    }
+    return res.status(400).json({message: "couldnot update the customer bucket log"});
+
 }
 return res.status(200).json(putIntoBucket);
 
