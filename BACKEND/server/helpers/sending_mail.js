1b7a7d0c523d61330f28d3ea0c1e422ad85cbf96
Bibek Regmi <bibek.regmi@swifttech.com>
2023-12-27 15:59:39 +0545

    database for customer_bucket and bucket_image created and send mail function made using nodemailer

---
+++ BACKEND/server/helpers/sending_mail.js
@@ -0,0 +1,68 @@
+"use strict";
+let nodemailer= require('nodemailer')
+let redisHelper = require('./redis_helper');
+const host = process.env.SMTPHOST;
+const port = process.env.SMTPPORT;
+const service = process.env.SMTPSERVICE;
+   
+const transporter =  nodemailer.createTransport({
+  host: host,
+  port: port,
+  secure: false,
+  service: service,
+  auth: {
+    user: process.env.USEREMAIL,
+    pass: process.env.PASSWORD,
+
+  },
+});
+exports.sendingMail = async(message)=>{
+  await transporter.sendMail({
+    to: message.email, //receiver email
+    subject: message.subject, // Subject line
+    text: message.details.message + ` ${message.details.value} ` // value is OTP 
+  });
+  
+
+if(message.type == 'OTP'){
+  let OTP=`${message.email}`;
+ await redisHelper.setDataToCache(OTP,message.details.value)
+  
+  return {
+  status: true,
+  OTP: message.details.value
+};
+}
+
+return  {
+  status: false,
+  value: 'UnsuccessFul'
+};
+
+  }
+
+
+
+  /*
+ // to be passed message format
+
+  const message = {
+        subject : `OTP verification Message`,
+        details : {
+          message: `This is an OTP to verify your transaction`,
+          //const OTP=Math.floor(Math.random() * (9999 - 1111) + 1111) //generates 4 digits random key
+          value: OTP 
+        },
+        email: email,
+        type:'OTP'
+      }
+  
+  
+  
+  
+  */ 
+
+  
+  
+  
+  
