11e642d3293f53ed19929530e089dfa05f997e96
abishek.paudel <abishek.paudel@swifttech.com.np>
2024-01-11 15:58:33 +0545

    changes in naming convention

--- BACKEND/server/modules/customer_post/sql/create.js
+++ BACKEND/server/modules/customer_post/sql/create.js
@@ -4,17 +4,17 @@ const httpStatus = require("http-status");
 const {v4} = require("uuid");
 
 (() => {
-    module.exports = async (call, res) => {
+    module.exports = async (body, res) => {
         try {
             let response = {status: httpStatus.BAD_REQUEST, message: "Data Not found"};
 
             let insertObj = {
                 uuid: v4(),
-                customer_id: call.user.uuid,
-                description: call.description,
-                image_name: call.image_name,
-                created_at: call.created_at,
-                updated_at: call.updated_at,
+                customer_id: body.user.uuid,
+                description: body.description,
+                image_name: body.image_name,
+                created_at: body.created_at,
+                updated_at: body.updated_at,
             };
 
             let query = await mysqlHelper.format(`INSERT INTO balance_humanity_blog_post SET ?`, [insertObj]);
--- BACKEND/server/routes/index.js
+++ BACKEND/server/routes/index.js
@@ -17,6 +17,6 @@ router.use("/balance", balance);
 
 //Customer posts CRUD
 const post = require("./customer_post/route.js");
-router.use("/post", post);
+router.use("/victim-post", post);
 
 module.exports = router;
