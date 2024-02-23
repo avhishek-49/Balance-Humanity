de62964bbf3222f9ae770be57935d99167162492
Salina Shrestha <salina.shrestha@nimble-cr.com>
2024-02-23 13:38:57 +0545

    campaign form start

--- FRONTEND/app/src/main.jsx
+++ FRONTEND/app/src/main.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './onboarding/login.jsx';
-import SignUpApplication from './onboarding/sign_up.jsx'; // Ensure the correct case
-import OtpCustomer from './onboarding/otp.jsx';
-import WelcomePage from './onboarding/welcome_page.jsx';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./onboarding/login.jsx";
+import SignUpApplication from "./onboarding/sign_up.jsx"; // Ensure the correct case
+import OtpCustomer from "./onboarding/otp.jsx";
+import WelcomePage from "./onboarding/welcome_page.jsx";
+import CampaignForm from "./components/CampaignForm.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
   },
   {
-    path: '/submit',
-    element: <SignUpApplication/>
+    path: "/submit",
+    element: <SignUpApplication />,
   },
   {
-    path: '/otp',
-    element: <OtpCustomer/>
+    path: "/otp",
+    element: <OtpCustomer />,
   },
   {
-    path: '/home',
-    element: <WelcomePage/>
+    path: "/home",
+    element: <WelcomePage />,
+  },
+  {
+    path: "/start-campaign",
+    element: <CampaignForm />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
--- FRONTEND/app/src/onboarding/welcome_page.jsx
+++ FRONTEND/app/src/onboarding/welcome_page.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useRef, useEffect } from "react";
 import NavBar from "../components/Navbar";
 import "./styles/welcome_page.css";
+import Layout from "../components/Layout";
 
 const humanityData = [
   {
     id: 1,
     title: "Test Title 1",
-    description: "Test description for balance humanity. 2 lines",
+    description: "Test description for balance humanity. 2-3 lines",
     image: "/images/test.jpg",
   },
   {
     id: 2,
     title: "Test Title 2",
-    description: "Test description for balance humanity. 2 lines",
+    description: "Test description for balance humanity. 2-3 lines",
     image: "/images/test.jpg",
   },
 ];
@@ -20,33 +21,34 @@ const humanityData = [
 const WelcomePage = () => {
   return (
     <>
-      <NavBar />
-      <div className="container-fluid main-page-wrapper">
-        <div className="main-page-banner">
-          <h1>
-            We Are In A Mission to Help <br /> The HelpLess <br /> Join Our
-            Action
-          </h1>
-        </div>
-        <div>
-          {humanityData?.map((item) => {
-            return (
-              <div class="custom-card card mb-3">
-                <img class="card-img" src={item.image} alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">{item.title}</h5>
-                  <p class="card-text">{item.description}</p>
-                  <p class="card-text">
-                    <button class="btn btn-success" type="submit">
-                      Donate Now
-                    </button>
-                  </p>
+      <Layout>
+        <div className="container-fluid main-page-wrapper">
+          <div className="main-page-banner">
+            <h1>
+              We Are In A Mission To Help <br /> The HelpLess <br /> Join Our
+              Action
+            </h1>
+          </div>
+          <div className="container">
+            {humanityData?.map((item) => {
+              return (
+                <div class="custom-card card mb-3">
+                  <img class="card-img" src={item.image} alt="Card image cap" />
+                  <div class="card-body">
+                    <h5 class="card-title">{item.title}</h5>
+                    <p class="card-text">{item.description}</p>
+                    <p class="card-text">
+                      <button class="btn btn-success" type="submit">
+                        Donate Now
+                      </button>
+                    </p>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
+      </Layout>
     </>
   );
 };
